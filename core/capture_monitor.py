0a1cd19aedf210f32fadcb4977e7491db691c1e2
Jeroen Pluimers <jeroen.bitbucket.org@pluimers.com>
2017-06-21 12:51:07 +0200

    Fix #1 "Invalid PCAP dump header. Probably not a valid capture file"
    Refactored hardcoded `http://fritz.box` references into dynamic ones.

--- core/capture_monitor.py
+++ core/capture_monitor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
-################################################################################# 
+#################################################################################
 # Simple FritzCap python port
 # Simplifies generation and examination of traces taken from AVM FritzBox and/or SpeedPort
 # Traces can be examined using WireShark
@@ -48,23 +48,23 @@ from string_helper import StringHelper
 from tracer import Tracer
 
 class CaptureMonitor(threading.Thread):
-    
+
     state_started = False
     next_stop_time = 0
     next_start_time = 0
     cap_file_path = ""
-        
-    def __init__(self, decode_work_queue, data_map, box_name, password, protocol, cap_folder, cap_file, login_required, default_login, sid_challenge, sid_login, start_str, stop_str, after_capture_time):        
+
+    def __init__(self, decode_work_queue, data_map, box_name, password, protocol, cap_folder, cap_file, login_required, default_login, sid_challenge, sid_login, start_str, stop_str, after_capture_time):
         threading.Thread.__init__(self)
-        self._stop = threading.Event()        
-        
+        self._stop = threading.Event()
+
         self.decode_work_queue = decode_work_queue
         self.data_map = data_map
         self.box_name = box_name
         self.password = password
         self.protocol = protocol
         self.cap_folder = cap_folder
-        self.cap_file = cap_file        
+        self.cap_file = cap_file
         self.login_required = login_required
         self.default_login = default_login
         self.sid_challenge = sid_challenge
@@ -73,7 +73,7 @@ class CaptureMonitor(threading.Thread):
         self.stop_str = stop_str
         self.after_capture_time = after_capture_time
         self.SID = ''
-        
+
         self.wait_condition = threading.Condition()
 
         self.logger = Log().getLogger()
@@ -81,12 +81,12 @@ class CaptureMonitor(threading.Thread):
 
     def run(self):
         self.logger.debug("Thread started.")
-        
+
         if self.login_required:
            if (not self.init_login()):
                self.logger.debug("Could not login. Stop the capture thread.")
                self._stop.set()
-        
+
         while not self._stop.isSet():
             ###################
             ### pre_capture ###
@@ -109,17 +109,17 @@ class CaptureMonitor(threading.Thread):
                     self.wait_condition.wait()
                     if (self.logger.isEnabledFor(logging.DEBUG)):
                         self.logger.debug("pre_capture wait() finished.")
-            
+
             self.logger.debug("pre_capture release lock.")
             self.wait_condition.release()
             self.logger.debug("pre_capture release lock finished.")
-            
+
             if (self._stop.isSet()):
                 logging.debug("The capture was not started. Not need to stop the capture. Can stop immediately.")
                 break
-            
+
             self.sub_start_capture()
-            
+
             ####################
             ### post_capture ###
             ####################
@@ -144,7 +144,7 @@ class CaptureMonitor(threading.Thread):
             self.logger.debug("post_capture release lock.")
             self.wait_condition.release()
             self.logger.debug("post_capture release lock finished.")
-            
+
             self.sub_stop_capture()
 
         if (self._stop.isSet()):
@@ -161,7 +161,7 @@ class CaptureMonitor(threading.Thread):
             self.logger.debug("Login attempt to the the FritzBox (box_name:%s)" % (self.box_name))
 
             # Try to get a session id SID
-            conn_url = 'http://fritz.box/login_sid.lua'
+            conn_url = self.protocol + '://' + self.box_name + '/login_sid.lua'
             self.logger.debug("Call the challange token url (url:'%s')" % conn_url)
             self.sid = urllib.urlopen(conn_url)
             if self.sid.getcode() == 200:
@@ -180,7 +180,7 @@ class CaptureMonitor(threading.Thread):
                 response_bf = challenge + '-' + m.hexdigest().lower()
 
                 # Answer the challenge
-                conn_url = 'http://fritz.box/login_sid.lua?username=root&response=' + response_bf
+                conn_url = self.protocol + '://' + self.box_name + '/login_sid.lua?username=root&response=' + response_bf
                 self.logger.debug("Call the read seed token url (url:'%s', data:'%s')." % (conn_url,self.sid_login % response_bf))
                 login = urllib.urlopen(conn_url)
 
@@ -208,7 +208,7 @@ class CaptureMonitor(threading.Thread):
                 except:
                     result = ''
                 self.logger.error('Login attempt was made, but something was wrong: %s' % result)
-        return False 
+        return False
 
     def init_capture_file(self):
         # Create capfile folder
@@ -216,16 +216,16 @@ class CaptureMonitor(threading.Thread):
         folder = folder.replace("\\","/")
         if (not folder.endswith("/")):
             folder = folder+"/"
-        
-        file = StringHelper.parse_string(self.cap_file, self.data_map)            
-        
+
+        file = StringHelper.parse_string(self.cap_file, self.data_map)
+
         self.cap_file_path = folder+file
         self.logger.debug("Initialize capture file (folder:%s, file:%s)." % (folder,file))
         if not os.path.exists(folder):
             self.logger.debug("Destination folder:'%s' not exists. Create." % folder)
-            os.makedirs(folder)        
-        
-    def sub_start_capture(self):          
+            os.makedirs(folder)
+
+    def sub_start_capture(self):
         # Start tracer thread, wait for console input to stop
         if self.login_required and not self.init_login():
             self.logger.debug("Could not login. Stop the capture thread.")
@@ -243,10 +243,10 @@ class CaptureMonitor(threading.Thread):
 
         self.logger.debug("Send capture start request to the box          (url:'%s', capture_file:'%s')." % (url, self.cap_file_path))
         Tracer(url, self.cap_file_path).start()
-        self.logger.debug("Send capture start request to the box finished (url:'%s', capture_file:'%s')." % (url, self.cap_file_path))        
-                
+        self.logger.debug("Send capture start request to the box finished (url:'%s', capture_file:'%s')." % (url, self.cap_file_path))
+
     def sub_stop_capture(self):
-        # Clean stop        
+        # Clean stop
         if self.login_required and not self.init_login():
             self.logger.debug("Could not login. Stop the capture thread.")
             self._stop.set()
@@ -258,31 +258,31 @@ class CaptureMonitor(threading.Thread):
             url = self.protocol + '://' + self.box_name + '/cgi-bin/capture_notimeout' + self.stop_str
         self.logger.debug("Send capture stop request to the box           (url:'%s', capture_file:'%s')." % (url, self.cap_file_path))
         urllib.urlopen(url)
-        self.logger.debug("Send capture stop request to the box finished  (url:'%s', capture_file:'%s')." % (url, self.cap_file_path))        
+        self.logger.debug("Send capture stop request to the box finished  (url:'%s', capture_file:'%s')." % (url, self.cap_file_path))
         self.logger.info("Capture finished (capture_file:'%s')." % (self.cap_file_path))
         self.set_data("tcape",datetime.datetime.now())
-        
+
         if (self.decode_work_queue is not None):
             self.logger.debug("Add captured file '%s' to the decoding work queue." % (self.cap_file_path))
             self.decode_work_queue.put(self.cap_file_path)
-                    
+
     def start_capture(self):
         self.wait_condition.acquire()
-        self.logger.debug("start_capture called.")        
+        self.logger.debug("start_capture called.")
         self.state_started = True
         self.wait_condition.notify_all()
         self.wait_condition.release()
 
     def stop_capture(self):
         self.wait_condition.acquire()
-        self.logger.debug("stop_capture called.")        
+        self.logger.debug("stop_capture called.")
         self.state_started = False
         if self.after_capture_time > 0:
             self.next_stop_time = time.time() + self.after_capture_time
         self.wait_condition.notify_all()
         self.wait_condition.release()
-    
-    
+
+
     def stop (self):
         self.logger.debug("Received signal to stop the thread.")
         self._stop.set()
@@ -294,7 +294,7 @@ class CaptureMonitor(threading.Thread):
 
     def stopped (self):
         return self._stop.isSet()
-    
+
     def set_callnumber(self, key, number):
         self.data_map[key+".number"] = number
         self.data_map[key+".name"] = ""
@@ -302,17 +302,14 @@ class CaptureMonitor(threading.Thread):
         if (self.data_map.has_key("pbook_number."+number)):
             self.data_map[key+".name"] = self.data_map.get("pbook_number."+number)
             self.data_map[key+".numbername"] = self.get_call_numbername(number)
-        
+
     def get_call_numbername(self, number):
         if (not number):
             number = "Unknown"
-        
+
         if (self.data_map.has_key("pbook_number."+number)):
             return number+"("+self.data_map.get("pbook_number."+number)+")"
         return number
-            
+
     def set_data(self, key, value):
         self.data_map[key]=value
-        
-    
-
